03e735eff4e4ebb2c1f89c6a04b6ae0c1a8435f7
Mahmut <can436436@gmail.com>
2020-10-27 17:08:12 +0300

    Update

--- MuslimWidget/Classes/Ayetler.cs
+++ MuslimWidget/Classes/Ayetler.cs
@@ -9,11 +9,23 @@ namespace MuslimWidget
         private string[] ayetler = new string[] { };
 
         public Ayetler()
-        {           
+        {
             if (!File.Exists(PathManager.data_ayetler_path))
-                ayetler = new string[] { "Hata: Veriler yüklenemedi." };
+            {
+                ayetler = new string[]
+                {
+                    "İki topluluğun karşı karşıya geldikleri gün, sizden geri dönenleri, kazandıkları bazı şeyler dolayısıyla şeytan onların ayağını kaydırmak istemişti. Ama andolsun ki, Allah onları affetti. Şüphesiz Allah, bağışlayandır, yumuşak olandır. (Al-i İmran Suresi, 155. Ayet).",
+                    "Kullarından tevbeyi kabul eden, kötülükleri affeden ve işlediklerinizi bilen O'dur. (Şura Suresi, 25. Ayet)",
+                    "Ve Allah`tan bağışlanma dile. Gerçekten Allah, bağışlayandır, esirgeyendir. (Nisa Suresi, 106. ayet)",
+                    "Rabbinin yoluna hikmetle ve güzel öğütle çağır ve onlarla en güzel bir biçimde mücadele et. Şüphesiz senin Rabbin yolundan sapanı bilendir ve hidayete ereni de bilendir. (NAHL suresi 125. ayet)",
+                    "Bir hayrı açıklar veya gizlerseniz, yahut bir kötülüğü affederseniz (bilin ki), Allah da çok affedicidir, her şeye hakkıyla gücü yetendir. (NİSA suresi 149. ayet)"
+                };
+                File.WriteAllLines(PathManager.data_ayetler_path,ayetler);
+            }
             else
+            { 
                 ayetler = File.ReadAllLines(PathManager.data_ayetler_path);
+            }
         }
         public string GetRandomAyet()
         {
--- MuslimWidget/Classes/Hadisler.cs
+++ MuslimWidget/Classes/Hadisler.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace MuslimWidget
 {
-   public class Hadisler
+    public class Hadisler
     {
 
         private string[] hadisler = new string[] { };
@@ -11,9 +11,20 @@ namespace MuslimWidget
         public Hadisler()
         {
             if (!File.Exists(PathManager.data_hadisler_path))
-                hadisler = new string[] { "Hata: veriler yüklenemedi." };
+            {
+                hadisler = new string[]
+                {
+                   "Her doğan, İslâm fıtratı üzerine doğar. Sonra, anne-babası onu Hristiyan, Yahudi veya Mecusi yapar. - (Buhârî, cenâiz 92; Ebû Dâvut, sünne 17; Tirmizî, kader 5)",
+                    "Müslüman, dilinden ve elinden Müslümanların emin olduğu kimsedir. Muhâcir de Allah’ın yasakladığı şeyleri terk edendir - Buhari, Bedu'l-vahy, 4.",
+                    "İnsanlara merhamet etmeyene Allah merhamet etmez - Müslim, Fedâil, 66; Tirmizî, Birr, 16",
+                    "Hayra vesile olan, hayrı yapan gibidir.- Tirmizî, İlim, 14."
+                };
+                File.WriteAllLines(PathManager.data_hadisler_path,hadisler);
+            }
             else
+            {
                 hadisler = File.ReadAllLines(PathManager.data_hadisler_path);
+            }
         }
         public string GetRandomHadis()
         {
